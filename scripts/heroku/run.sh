3370e0254c71fbc53cc37dcb9f4a862ee9c9f315
whywaita <whywaita@whywrite.it>
2020-12-07 19:11:54 +0900

    not use heroku

--- scripts/heroku/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-protoc -I . *.proto --go_out=plugins=grpc:.
-
-## setup, build binaries to app root directory
-ls plugins | tr -d "/" | xargs -I%% bash -c "cd plugins/%% ; go build -o ../../%% . ; cd ../../"
-
-go run cmd/server/...
